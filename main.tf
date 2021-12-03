c961ebda1a4af84165af1dc4e4fc88eaf8a0f840
srinivasan-chandran <95075559+srinivasan-chandran@users.noreply.github.com>
2021-12-03 16:49:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ data "vsphere_virtual_machine" "template" {
 }
 
 
-resource "vsphere_virtual_machine" "cloned_virtual_machine" {
+resource "vsphere_virtual_machine" "centos7_64Guest" {
   name             = var.vsphere_vm_name
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
