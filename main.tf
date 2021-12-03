921110833547b37be1590076a989bc2ae4c0e2bf
srinivasan-chandran <95075559+srinivasan-chandran@users.noreply.github.com>
2021-12-03 16:46:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ data "vsphere_virtual_machine" "template" {
 }
 
 
-resource "vsphere_virtual_machine" "vm" {
+resource "vsphere_virtual_machine" "cloned_virtual_machine" {
   name             = var.vsphere_vm_name
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
